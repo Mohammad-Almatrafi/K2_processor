17421ac298d32518f18333f883df2b6385b03413
Rur1k <moalmatrafi90@gmail.com>
2024-11-23 14:09:17 +0300

    added instruction comments

--- src/Programs/modules/DataTransferProgram.sv
+++ src/Programs/modules/DataTransferProgram.sv
@@ -10,17 +10,17 @@ module DataTransferProgram (
 
   always @(s) begin
     case (s)
-      0:  out = {4'h0, {1'b1, 3'd2}};
-      1:  out = {4'hf, {1'b1, 3'd0}};
-      2:  out = {4'h0, {1'b1, 3'd5}};
-      3:  out = {4'hf, {1'b1, 3'd1}};
-      4:  out = {4'hc, {1'b1, 3'd0}};
-      5:  out = {4'h2, {1'b0, 3'd0}};
-      6:  out = {4'hc, {1'b1, 3'd1}};
-      7:  out = {4'h2, {1'b0, 3'd0}};
-      8:  out = {4'h1, {1'b1, 3'd5}};
-      9:  out = {4'h0, {1'b0, 3'd4}};
-      10: out = {8'hf, {1'b0, 3'd0}};
+      0:  out = {4'h0, {1'b1, 3'd2}}; // Ra = 2
+      1:  out = {4'hf, {1'b1, 3'd0}}; // [0] = Ra
+      2:  out = {4'h0, {1'b1, 3'd5}}; // Ra = 5
+      3:  out = {4'hf, {1'b1, 3'd1}}; // [1] = Ra
+      4:  out = {4'hc, {1'b1, 3'd0}}; // Ra = [0]
+      5:  out = {4'h2, {1'b0, 3'd0}}; // Ro = Ra
+      6:  out = {4'hc, {1'b1, 3'd1}}; // Ra = [1]
+      7:  out = {4'h2, {1'b0, 3'd0}}; // Ro = Ra
+      8:  out = {4'h1, {1'b1, 3'd5}}; // Rb = 5
+      9:  out = {4'h0, {1'b0, 3'd4}}; // Ra = Ra - Rb
+      10: out = {8'hf, {1'b0, 3'd0}}; // Jz 0
     endcase
   end
 
--- src/Programs/modules/fibbonacciSequenceProgram.sv
+++ src/Programs/modules/fibbonacciSequenceProgram.sv
@@ -9,15 +9,15 @@ module fibbonacciSequenceProgram (
 
   always @(s) begin
     case (s)
-      0: out = 8'b0000_1000;
-      1: out = 8'b0001_1001;
-      2: out = 8'b0010_0000;
-      3: out = 8'b0001_0000;
-      4: out = 8'b0111_0000;
-      5: out = 8'b0000_0000;
-      6: out = 8'b0001_0100;
-      7: out = 8'b0000_0100;
-      8: out = 8'b1011_0010;
+      0: out = 8'b0000_1000; // Ra = 0
+      1: out = 8'b0001_1001; // Rb = 1
+      2: out = 8'b0010_0000; // Ro = Ra
+      3: out = 8'b0001_0000; // Rb = Ra + Rb
+      4: out = 8'b0111_0000; // JC 0
+      5: out = 8'b0000_0000; // Ra = Ra + Rb
+      6: out = 8'b0001_0100; // Rb = Ra - Rb
+      7: out = 8'b0000_0100; // Ra = Ra - Rb
+      8: out = 8'b1011_0010; // J 2
     endcase
   end
 
